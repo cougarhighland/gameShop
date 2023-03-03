2c7aa79e6775fa308a27cd4dcaceeb13e196632a
xuan tran <xuandundkl@gmail.com>
2023-03-03 21:08:19 +0100

    first commit and info of products

---
+++ src/data/products.js
@@ -0,0 +1,81 @@
+const products = [
+    {
+        productNumber: "1",
+        name: "Grand thefts Auto V",
+        price: "298kr",
+        description:"super fun open-world game to play with friends",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "2",
+        name: "The Legend of Zelda: Breath of the Wild",
+        price: "600kr",
+        description:"the open world game that a hero will save the world from evil",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "3",
+        name: "Portal 2",
+        price: "10kr",
+        description:"The Perpetual Testing Initiative has been expanded to allow you to design co-op puzzles for you and your friends!",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "4",
+        name: "Half-life 2",
+        price: "97.5kr",
+        description:"a first-person shooter game",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "5",
+        name: "Disco Elysium",
+        price: "399kr",
+        description:"a noir detective fiction, traditional pen-and-paper RPGs game",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "6",
+        name: "Hades",
+        price: "250kr",
+        description:"a incredible action game about Greek mythology",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "7",
+        name: "The Witcher 3: Wild Hunt",
+        price: "299kr",
+        description:"An RPG game about fantasy world filled with monsters, magic and witcher",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "8",
+        name: "The Last of Us",
+        price: "599kr",
+        description:"Experience the emotional storytelling and unforgettable characters in The Last of Us™, winner of over 200 Game of the Year awards",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "9",
+        name: "Bioshock",
+        price: "199kr",
+        description:"BioShock is a shooter unlike any you've ever played, loaded with weapons and tactics never seen",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "9",
+        name: "Dark souls 3",
+        price: "599kr",
+        description:"fast-pace and hardcore RPG game , prepare yourself and Embrace The Darkness!",
+        imgPath: "/assets/img/1.png",
+      },
+      {
+        productNumber: "10",
+        name: "God of War",
+        price: "499kr",
+        description:"an adventure game to Norse realm, exploring story between father and son",
+        imgPath: "/assets/img/1.png",
+      }
+]
+
+export default products;
